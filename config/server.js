c1c3425da2be518747a62934f06b9d0cbb8a8afa
Rifky azad <68014543+Rifky-azad@users.noreply.github.com>
2021-06-13 16:39:14 +0530

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b26249469bb493e805db1f7b0b9ad287'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4de6af84-51f3-4914-bd57-153243fffa33'
+};
